5af34709fd18fce1121a4d3226e27e4b7f2c0320
Fredrik Anerdin <iot@anerdins.se>
2020-06-22 20:22:31 +0200

    Test with multiple registers every 2 seconds

--- modbus-client.js
+++ modbus-client.js
@@ -3,7 +3,7 @@ var ModbusRTU = require("modbus-serial");
 var client = new ModbusRTU();
  
 // open connection to a tcp line
-client.connectTCP("127.0.0.1", { port: 8502 });
+client.connectTCP("192.168.1.23", { port: 502 });
 client.setID(1);
 
 // read the values of 10 registers starting at address 0
